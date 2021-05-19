7a6305c91f39125e09346c956c49d9306e97bb8e
Vladimir Agapov <kyleridrus@gmail.com>
2021-05-20 00:28:22 +0300

    remove comments

--- extractor.py
+++ extractor.py
@@ -2,51 +2,6 @@
 # import pandas as pd
 from docx.api import Document
 import glob
-import numpy as np
-import itertools
-from collections import OrderedDict
-
-
-def bubble_sort(array):
-    print('---bubble sort---')
-    n = len(array)
-
-    for i in range(n):
-        # Create a flag that will allow the function to
-        # terminate early if there's nothing left to sort
-        already_sorted = True
-
-        # Start looking at each item of the list one by one,
-        # comparing it with its adjacent value. With each
-        # iteration, the portion of the array that you look at
-        # shrinks because the remaining items have already been
-        # sorted.
-        for j in range(n - i - 1):
-            print('array j', array[j])
-            if array[j][3][1] > array[j + 1][3][1]:
-                # If the item you're looking at is greater than its
-                # adjacent value, then swap them
-                temp = list(array[j][3][1])
-                array[j][3][1] = array[j + 1][3][1]
-                array[j + 1][3][1] = tuple(temp)
-
-                # array[j][3][1], array[j + 1][3][1] = array[j + 1][3][1], array[j][3][1]
-
-
-
-                # Since you had to swap two elements,
-                # set the `already_sorted` flag to `False` so the
-                # algorithm doesn't finish prematurely
-                already_sorted = False
-
-        # If there were no swaps during the last iteration,
-        # the array is already sorted, and you can terminate
-        if already_sorted:
-            break
-
-    return array
-
-
 
 
 
@@ -64,13 +19,6 @@ for i in docFiles:
     document = Document(i)
     table = document.tables[0]
 
-
-    # for row in table.rows:
-    #     for cell in row.cells:
-    #         for para in cell.paragraphs:
-    #             print(para.text)
-
-
     keys = None
     for i, row in enumerate(table.rows):
         text = (cell.text for cell in row.cells)
@@ -85,58 +33,25 @@ for i in docFiles:
             data.append(row_data)
         print(counter)
 
-
-# print(type(data), data[0], type(data[0]))
-doubles = []
+unique = []
 
 for i in data:
-    if i not in doubles:
-        doubles.append(i)
+    if i not in unique:
+        unique.append(i)
     else:
         print(i,end=' ')
-# print('end', len(doubles))
-# i = 0
-# while i < len(data):
-#     j = i + 1
-#     print('i:', i)
-#     while j < len(data):
-#         print('j:', j)
-#         if data[i] == data[j]:
-#             doubles.append(data[i])
-#             break
-#         j += 1
-#     i += 1
-
-# print(len(doubles))
-# for item in data: #row
-#     print(type(item))
-#     for i in data:
-#         if (item == i) {
-#             doubles
-#         }
-    # for el in item: #cell
-        # print(el)
 
 targetDoc = Document('C:\\Users\\KyleRid\\Desktop\\py\\source\\target.docx')
 targetTable = targetDoc.tables[0]
 globalCounter = 1
-# # # add a data row for each item
 
-# newData = bubble_sort(data)
-data.sort(key = lambda x: x[3])
+# data = unique
+data.sort(key = lambda x: x[3]) #in place sorting
 
-# res = list(set([ele for ele in data if data.count(ele) > 1]))
-
-
-# print(res)
-
-
-# for item in data:
 
 print(len(data))
-# print(type(newData))
+
 for item in data:
-    # print(item[3][1])
     print(globalCounter)
     cells = targetTable.add_row().cells
     cells[0].text = str(globalCounter)
@@ -149,7 +64,6 @@ for item in data:
     cells[7].text = item[7][1]
     cells[8].text = item[8][1]
     cells[9].text = item[9][1]
-    # print(cells[8].text)
     globalCounter = globalCounter + 1
 
 targetDoc.save('C:\\Users\\KyleRid\\Desktop\\py\\source\\target2.docx')
